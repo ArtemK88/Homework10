a9f7ae59e00abc142d0680e6ff81881c452be244
ArtemK88 <kapiton2007@gmail.com>
2023-06-14 21:55:53 +0300

    Homework

--- src/Dublicate.java
+++ src/Dublicate.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Dublicate {
+//1.Написати програму яка видаляє з колекції цілих чисел всі дублікати, якщо вони є.
+// Початкову колекцію з 100 елементів потрібно заповнити випадковими числами (ліміт 50).
+// На екран потрібно вивести обидві колекції та кількість видалених дублікатів. (За бажанням можна вирішити декількома способами та порівняти, який з них швидше)
+
+import java.util.*;
+
+public class Dublicate {
+    public static List<Integer> randomValueWithLimit(int size, int limit) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(limit));
+        }
+//        Collections.sort(list);
+        return list;
+    }
+
+    public static List<Integer> removeDublicatesFirst(List<Integer> list) {
+        List<Integer> lI = new ArrayList<>();
+        for (Integer list1 : list) {
+            if (!lI.contains(list1)) {
+                lI.add(list1);
+            }
+        }
+        return lI;
+
+    }
+
+    static List<Integer> removeDuplicatesSecond(List<Integer> list) {
+        Set<Integer> set = new HashSet<>(list);
+        return new ArrayList<>(set);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list1 = Dublicate.randomValueWithLimit (100,50);
+        System.out.println("Оригінальна таблиця " + list1);
+        System.out.println();
+        List<Integer> list2 = Dublicate.removeDublicatesFirst(list1);
+        System.out.println("Таблиця без дублікатів first " + list2);
+        System.out.println("Кількість дублікатів " + (list1.size() - list2.size()));
+        System.out.println();
+        List<Integer> list3 = Dublicate.removeDuplicatesSecond(list1);
+        System.out.println("Таблиця без дублікатів second " + list3);
+        System.out.println("Кількість дублікатів " + (list1.size() - list3.size()));
     }
 }
